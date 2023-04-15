95fad61fa259bca1640058a8e36c5e5d2a804d01
siva <sksivaiah759@gmail.com>
2023-04-15 10:45:19 +0000

    done

---
+++ names/index.js
@@ -0,0 +1,6 @@
+const n = require("../country/state/city/index.js");
+const o = require("../utilities/utils/index.js");
+function getPeopleInCity(n) {
+  return o(n);
+}
+module.exports = getPeopleInCity;
